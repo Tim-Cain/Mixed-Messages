d4aa438bc0ff29546ebdf21591dfbe9b5050dc97
tim-cain <tim-cain@hotmail.co.uk>
2022-05-16 12:13:05 +0100

    Test3

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,7 +1,7 @@
 function messageGen(num){
-    if (num == 0){return "Message1";}
-     else if (num == 1){return "Message2";}
-     else if (num == 2){return "message3";}
+    if (num == 1){return "Message1";}
+     else if (num == 2){return "Message2";}
+     else if (num == 3){return "message3";}
 }
 
 function numGen(){
